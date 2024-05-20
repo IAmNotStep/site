73a7a50fbf0ef29010e05d7a77a7a483b9fe5c75
Stephy <stephyzhang2006@gmail.com>
2024-05-19 22:23:52 -0400

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -4,12 +4,12 @@ import { useState } from "react";
 
 export default function Home() {
   const images = [
-    "/20240329114256_IMG_4951.JPG",
-    "/20240329114420_IMG_4953.JPG"
+    "/IMG_3671.jpg",
+    "/IMG_3704.jpg"
   ]
   const text = [
-    "blurb about image 1",
-    "blurb about image 2"
+    "This is my favorite cat in the cafe",
+    "I shook hands with this cat"
   ]
 
   const [hoverIndex, setHoverIndex] = useState(null);
@@ -18,7 +18,7 @@ export default function Home() {
     <main className="relative h-screen bg-gradient-to-b from-indigo-500 to-orange-600">
       <div style={{ display: 'flex', justifyContent: 'space-between' }}>
         <div style={{ flex: '0 0 40%', marginRight: '1rem', display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
-          <h1 style={{ fontSize: '24px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>This is random text say hi or something lol.</h1>
+          <h1 style={{ fontSize: '24px', textShadow: '2px 2px 4px rgba(0, 0, 0, 0.5)' }}>Hi, these are some pictures from the cat cafe I went</h1>
         </div>
         <div style={{ flex: '1', display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))', gap: '1rem' }}>
           {images.map((image, index) => (
